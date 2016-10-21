b18cfa5df477fac61ac62c283bf9c471b3f104f4
lidox <schaefa@mail.ru>
2016-10-21 11:46:26 +0200

    remove second toolbar

--- NCCN/app/src/main/java/com/artursworld/nccn/view/Launcher.java
+++ NCCN/app/src/main/java/com/artursworld/nccn/view/Launcher.java
@@ -3,6 +3,8 @@ package com.artursworld.nccn.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 import com.artursworld.nccn.R;
 import com.artursworld.nccn.controller.wizard.WizardNCCN;
@@ -13,6 +15,9 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setVisibility(View.GONE);
     }
 
     @Override
