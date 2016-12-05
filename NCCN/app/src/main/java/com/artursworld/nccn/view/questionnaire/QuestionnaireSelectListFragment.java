0b80ed0b2ab04878264ccc7779a277af2ea547bf
lidox <schaefa@mail.ru>
2016-12-05 12:33:14 +0100

    add connection between launcher and the questionnaires

--- NCCN/app/src/main/java/com/artursworld/nccn/view/questionnaire/QuestionnaireSelectListFragment.java
+++ NCCN/app/src/main/java/com/artursworld/nccn/view/questionnaire/QuestionnaireSelectListFragment.java
@@ -1,5 +1,6 @@
 package com.artursworld.nccn.view.questionnaire;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,7 +15,18 @@ import android.widget.TextView;
 
 import com.artursworld.nccn.R;
 import com.artursworld.nccn.controller.util.Strings;
+import com.artursworld.nccn.controller.wizard.WizardHADSD;
+import com.artursworld.nccn.controller.wizard.WizardNCCN;
+import com.artursworld.nccn.controller.wizard.WizardQualityOfLife;
 import com.artursworld.nccn.model.entity.AbstractQuestionnaire;
+import com.artursworld.nccn.model.entity.DistressThermometerQuestionnaire;
+import com.artursworld.nccn.model.entity.HADSDQuestionnaire;
+import com.artursworld.nccn.model.entity.QolQuestionnaire;
+import com.artursworld.nccn.model.entity.User;
+import com.artursworld.nccn.model.persistence.manager.DistressThermometerQuestionnaireManager;
+import com.artursworld.nccn.model.persistence.manager.HADSDQuestionnaireManager;
+import com.artursworld.nccn.model.persistence.manager.QualityOfLifeManager;
+import com.artursworld.nccn.model.persistence.manager.UserManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,6 +72,28 @@ public class QuestionnaireSelectListFragment extends Fragment {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Log.i(CLASS_NAME, "selected medicament=" + abstractQuestionnairesList.get(position) + " at position: " + position);
+
+                    //TODO: stuff to delete and manage
+                    UserManager m = new UserManager();
+                    String userName = "Ahmet";
+                    m.insertUser(new User(userName));
+                    DistressThermometerQuestionnaireManager qm = new DistressThermometerQuestionnaireManager();
+                    qm.insertQuestionnaire(new DistressThermometerQuestionnaire(userName));
+                    QualityOfLifeManager qm1 = new QualityOfLifeManager();
+                    qm1.insertQuestionnaire(new QolQuestionnaire(userName));
+                    HADSDQuestionnaireManager qm2 = new HADSDQuestionnaireManager();
+                    qm2.insertQuestionnaire(new HADSDQuestionnaire(userName));
+
+
+                    if(position == 0){
+                        startActivityForResult(new Intent(getContext(), WizardHADSD.class), 2);
+                    }
+                    else if (position == 1){
+                        startActivityForResult(new Intent(getContext(), WizardNCCN.class), 1);
+                    }
+                    else if (position == 2){
+                        startActivityForResult(new Intent(getContext(), WizardQualityOfLife.class), 3);
+                    }
                 }
             });
         }
