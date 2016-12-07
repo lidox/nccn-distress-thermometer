4f91526a803b87f43b8cd96e3a6ef3d49025778f
lidox <schaefa@mail.ru>
2016-12-07 15:11:49 +0100

    fix some bugs

--- NCCN/app/src/main/java/com/artursworld/nccn/view/user/UserStartConfiguration.java
+++ NCCN/app/src/main/java/com/artursworld/nccn/view/user/UserStartConfiguration.java
@@ -14,7 +14,6 @@ import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.artursworld.nccn.R;
 import com.artursworld.nccn.controller.util.Strings;
-import com.fenjuly.mylibrary.ToggleExpandLayout;
 
 public class UserStartConfiguration {
 
@@ -29,7 +28,7 @@ public class UserStartConfiguration {
     }
 
     public void showConfigurationDialog(){
-        dialog = new MaterialDialog.Builder(cxt)
+        MaterialDialog.Builder b = new MaterialDialog.Builder(cxt)
                 .title(R.string.questionnaire_to_display)
                 .positiveText(R.string.ok)
                 .negativeText(R.string.cancel)
@@ -40,33 +39,36 @@ public class UserStartConfiguration {
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         Log.i(CLASS_NAME, "ok");
                     }
-                })
-                .show();
-        addToggleBtnListener();
-    }
+                });
+        dialog = b.show();
+        toggleById(R.id.toggle_user_layout, R.id.layout_users, R.string.select_existing_user, R.string.switch_to_new_user,  R.string.create_new_user, R.string.switch_to_existing_user);
+        toggleById(R.id.included_questionnaire_title, R.id.layout_below_included_questionnaire_title, R.string.configurable_questionnaire, R.string.switch_to_default_selection,R.string.standard_questionnaire, R.string.switch_to_questionnaire_selection);
+        }
 
-    private void addToggleBtnListener() {
-        //toggleById(R.id.toggle_user_layout, R.string.configurable_questionnaire, R.string.switch_to_default_selection,  R.string.standard_questionnaire, R.string.switch_to_questionnaire_selection);
-        //toggleById(R.id.questionnaire_layout, R.id.layout_questionnaires, R.string.switch_to_default_selection,   R.string.standard_questionnaire, R.string.switch_to_questionnaire_selection);
-        toggleById(R.id.toggle_user_layout, R.id.layout_users, R.string.create_new_user, R.string.switch_to_existing_user,  R.string.select_existing_user, R.string.switch_to_new_user);
-        toggleById(R.id.questionnaire_layout, R.id.layout_questionnaires, R.string.standard_questionnaire, R.string.switch_to_questionnaire_selection,  R.string.configurable_questionnaire, R.string.switch_to_default_selection);
-
-    }
-
-    private void toggleById(int toggleUserLayout, final int layoutToToggle, final int titleTextId, final int subTitleTextId, final int invisibleTitleTextId, final int invisibleSubTitleTextId){
-        final View toggleTitleLayout =  dialog.getView().findViewById(toggleUserLayout);
+    /**
+     * Toggle View Content which is below Title and Subtitle
+     * @param barToggleLayoutId the id of included title, subtitle and switcher layout
+     * @param layoutIdBelowBarToggleLayout  the id of layout below included title, subtitle and switcher layout
+     * @param titleTextId the title displayed if toggle button is checked
+     * @param subTitleTextId the subtitle displayed if toggle button is checked
+     * @param invisibleTitleTextId the title displayed if toggle button is unchecked
+     * @param invisibleSubTitleTextId the subtitle displayed if toggle button is unchecked
+     */
+    private void toggleById(int barToggleLayoutId, final int layoutIdBelowBarToggleLayout, final int titleTextId, final int subTitleTextId, final int invisibleTitleTextId, final int invisibleSubTitleTextId){
+        final View toggleTitleLayout =  dialog.getView().findViewById(barToggleLayoutId);
         Switch questionnaireSwitch = (Switch) toggleTitleLayout.findViewById(R.id.switcher);
 
         // init UI
-        final TextView title = (TextView) dialog.getView().findViewById(R.id.title);
-        final TextView subtitle = (TextView) dialog.getView().findViewById(R.id.subtitle);
-        title.setText(Strings.getStringByRId(titleTextId));
-        subtitle.setText(Strings.getStringByRId(subTitleTextId));
+        final TextView title = (TextView) toggleTitleLayout.findViewById(R.id.title);
+        final TextView subtitle = (TextView) toggleTitleLayout.findViewById(R.id.subtitle);
+        title.setText(Strings.getStringByRId(invisibleTitleTextId));
+        subtitle.setText(Strings.getStringByRId(invisibleSubTitleTextId));
 
         questionnaireSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                RelativeLayout layout = (RelativeLayout) dialog.getView().findViewById(layoutToToggle);
+                RelativeLayout layout = (RelativeLayout) dialog.getView().findViewById(layoutIdBelowBarToggleLayout);
+
                 // show details UI
                 if(isChecked){
                     layout.setVisibility(View.VISIBLE);
