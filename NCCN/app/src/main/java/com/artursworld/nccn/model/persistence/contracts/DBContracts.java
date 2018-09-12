c6bebed1c67bfef2a2692afd537da6768be4ec52
lidox <schaefa@mail.ru>
2018-09-12 17:39:59 +0200

    add contract

--- NCCN/app/src/main/java/com/artursworld/nccn/model/persistence/contracts/DBContracts.java
+++ NCCN/app/src/main/java/com/artursworld/nccn/model/persistence/contracts/DBContracts.java
@@ -3,7 +3,6 @@ package com.artursworld.nccn.model.persistence.contracts;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 
 public class DBContracts {
@@ -82,6 +81,28 @@ public class DBContracts {
         public static final String OPERATION_DATE = "operation_date";
     }
 
+    // Fear of Progression Table
+    public static class FoPTable {
+        public static final String TABLE_NAME = "fear_of_progression";
+        public static final String COLUMN_NAME_ID_FK = "user_id"; //foreign key
+        public static final String CREATION_DATE_PK = "creation_date";
+        public static final String UPDATE_DATE = "update_date";
+        public static final String PROGRESS = "progress_of_questionnaire";
+        public static final String LAST_QUESTION_EDITED_NR = "nr_of_last_question_edited";
+        public static final String ANSWER_1 = "ANSWER_TO_QUESTION1";
+        public static final String ANSWER_2 = "ANSWER_TO_QUESTION2";
+        public static final String ANSWER_3 = "ANSWER_TO_QUESTION3";
+        public static final String ANSWER_4 = "ANSWER_TO_QUESTION4";
+        public static final String ANSWER_5 = "ANSWER_TO_QUESTION5";
+        public static final String ANSWER_6 = "ANSWER_TO_QUESTION6";
+        public static final String ANSWER_7 = "ANSWER_TO_QUESTION7";
+        public static final String ANSWER_8 = "ANSWER_TO_QUESTION8";
+        public static final String ANSWER_9 = "ANSWER_TO_QUESTION9";
+        public static final String ANSWER_10 = "ANSWER_TO_QUESTION10";
+        public static final String ANSWER_11 = "ANSWER_TO_QUESTION11";
+        public static final String ANSWER_12 = "ANSWER_TO_QUESTION12";
+    }
+
     // Create SQL queries
     public static final String CREATE_META_QUESTIONNAIRE_TABLE = "CREATE TABLE "
             + MetaQuestionnaireTable.TABLE_NAME + "("
@@ -152,10 +173,34 @@ public class DBContracts {
             + "FOREIGN KEY(" + DistressThermometerTable.NAME_ID_FK + ") "
             + "REFERENCES " + UserTable.TABLE_NAME + "(" + UserTable.NAME_ID_PK + ") ON DELETE CASCADE ON UPDATE CASCADE);";
 
+    // CREATE FEAR OF PROGRESSION
+    private static final String SQL_CREATE_FEAR_OF_PROGRESSION_TABLE =
+            "CREATE TABLE " + FoPTable.TABLE_NAME + " ("
+                    + FoPTable.CREATION_DATE_PK + DATE_TYPE + COMMA_SEP
+                    + FoPTable.COLUMN_NAME_ID_FK + TEXT_TYPE + COMMA_SEP
+                    + FoPTable.UPDATE_DATE + DATE_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_1 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_2 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_3 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_4 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_5 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_6 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_7 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_8 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_9 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_10 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_11 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.ANSWER_12 + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.LAST_QUESTION_EDITED_NR + INTEGER_TYPE + COMMA_SEP
+                    + FoPTable.PROGRESS + INTEGER_TYPE + COMMA_SEP
+                    + " PRIMARY KEY (" + FoPTable.CREATION_DATE_PK + ")"
+                    + " FOREIGN KEY (" + FoPTable.COLUMN_NAME_ID_FK + ")"
+                    + " REFERENCES " + UserTable.TABLE_NAME + "(" + UserTable.NAME_ID_PK + ") ON DELETE CASCADE ON UPDATE CASCADE);";
+
     // Helper class manages database creation and version management
     public static class DatabaseHelper extends SQLiteOpenHelper {
 
-        private static final int DATABASE_VERSION = 3;
+        private static final int DATABASE_VERSION = 4;
         private static final String DATABASE_NAME = "app.db";
         private static DatabaseHelper instance;
 
@@ -175,6 +220,7 @@ public class DBContracts {
             db.execSQL(CREATE_HADSD_TABLE);
             db.execSQL(CREATE_QUALITY_OF_TABLE);
             db.execSQL(CREATE_DISTRESS_THERMOMETER_TABLE);
+            db.execSQL(SQL_CREATE_FEAR_OF_PROGRESSION_TABLE);
             db.execSQL(CREATE_META_QUESTIONNAIRE_TABLE);
         }
 
@@ -182,8 +228,9 @@ public class DBContracts {
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // Whenever you design a newer version, make sure to add some migration here.
             String upgradeQuery = "ALTER TABLE " + MetaQuestionnaireTable.TABLE_NAME + " ADD COLUMN " + MetaQuestionnaireTable.HAD_ALREADY_PSYCHOSOCIAL_SUPPORT + " " + TEXT_TYPE;
-            if (oldVersion == 2 && newVersion == 3)
+            if (oldVersion == 2 && newVersion == 3) {
                 db.execSQL(upgradeQuery);
+            }
         }
 
     }
